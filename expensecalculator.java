92639c30d18e39c71056be2fd5bfe2a322d1b0b5
KoPS-14 <kovendanable@gmail.com>
2025-05-20 13:14:48 +0530

    Create expensecalculator.java

---
+++ expensecalculator.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class expensecalculator{
+  public static void main(String args[]){
+     int incom,expense,amount;
+     Scanner s=new Scanner(System.in);
+     incom=s.nextInt();
+     expense=s.nextInt();
+     amount=incom-expense;
+     System.out.print("amount);
+  }
+}
